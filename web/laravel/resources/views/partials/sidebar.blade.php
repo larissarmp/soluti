e433666047261e68cb73ed113368fd8e1d3016f2
larissaribeiro <larissa@umobi.com.br>
2021-01-09 18:40:26 -0300

    projeto soluti

--- web/laravel/resources/views/partials/sidebar.blade.php
+++ web/laravel/resources/views/partials/sidebar.blade.php
@@ -11,41 +11,6 @@
                     <span class="title">@lang('quickadmin.qa_dashboard')</span>
                 </a>
             </li>
-
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-users"></i>
-                    <span class="title">@lang('quickadmin.user-management.title')</span>
-                    <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-                </a>
-                <ul class="treeview-menu">
-                    <li>
-                        <router-link :to="{ name: 'roles.index' }">
-                            <i class="fa fa-briefcase"></i>
-                            <span class="title">
-                                @lang('quickadmin.roles.title')
-                            </span>
-                        </router-link>
-                    </li>
-                    <li>
-                        <router-link :to="{ name: 'users.index' }">
-                            <i class="fa fa-user"></i>
-                            <span class="title">
-                                @lang('quickadmin.users.title')
-                            </span>
-                        </router-link>
-                    </li>
-                
-                </ul>
-            </li>
-            <li>
-                <router-link :to="{ name: 'companies.index' }">
-                    <i class="fa fa-gears"></i>
-                    <span class="title">@lang('quickadmin.companies.title')</span>
-                </router-link>
-            </li>
              <li>
                 <router-link :to="{ name: 'group.index' }">
                     <i class="fa fa-gears"></i>
@@ -64,13 +29,6 @@
                     <span class="title">@lang('quickadmin.upload.title')</span>
                 </router-link>
             </li>
-            <li>
-                <router-link :to="{ name: 'employees.index' }">
-                    <i class="fa fa-gears"></i>
-                    <span class="title">@lang('quickadmin.employees.title')</span>
-                </router-link>
-            </li>
-
             <li>
                 <router-link :to="{ name: 'auth.change_password' }">
                     <i class="fa fa-key"></i>
